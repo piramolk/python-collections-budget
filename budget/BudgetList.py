504447b845dbb3be9231b23e1589864f826addab
Piramol Krishnan <piramolkk@gmail.com>
2022-09-04 22:29:58 +0800

    ADD: read in file, added data to BudgetList and printed output

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,3 +1,5 @@
+from budget import Expense
+
 class BudgetList:
     def __init__(self,budget):
         self.budget = budget
@@ -10,7 +12,20 @@ class BudgetList:
         if(self.sum_expenses+item < self.budget):
             self.expenses.append(item)
             self.sum_expenses += item
+        else:
+            self.overages.append(item)
+            self.sum_overages += item
+    
+    def __len__(self):
+        return len(self.expenses) + len(self.overages)
 
+    def main():
+        myBudgetList = BudgetList(1200)
+        expenses = Expense.Expenses()
+        expenses.read_expenses('data/spending_data.csv')
 
+        for expense in expenses.list:
+            myBudgetList.append(expense.amount)
+        print('The count of all expenses: ' + str(len(myBudgetList))) 
 
 
