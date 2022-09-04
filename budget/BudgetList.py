0d5fd3f5fcb560e8a4f818baf1bc851f71a385c3
Piramol Krishnan <piramolkk@gmail.com>
2022-09-04 22:31:55 +0800

    EDIT: moved main() function definition outside class

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -19,13 +19,14 @@ class BudgetList:
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
-    def main():
-        myBudgetList = BudgetList(1200)
-        expenses = Expense.Expenses()
-        expenses.read_expenses('data/spending_data.csv')
 
-        for expense in expenses.list:
-            myBudgetList.append(expense.amount)
-        print('The count of all expenses: ' + str(len(myBudgetList))) 
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+    print('The count of all expenses: ' + str(len(myBudgetList))) 
 
 
