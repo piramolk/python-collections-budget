291804952c5cc7a74f5578a039ab208b984b191e
Piramol Krishnan <piramolkk@gmail.com>
2022-09-04 22:10:23 +0800

    ADD: obtained the top5 categories and plotted a histogram in matplotlib

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,4 +1,6 @@
-import Expense
+from budget import Expense
+import collections 
+import matplotlib.pyplot as plt
 
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
@@ -7,4 +9,15 @@ spending_categories = []
 
 for expense in expenses.list:
     spending_categories.append(expense.category)
-    print('check')
+
+spending_counter = collections.Counter(spending_categories)
+
+top5 = spending_counter.most_common(5)
+
+categories, count = zip(*top5)
+
+fig, ax = plt.subplot()
+ax.bar(categories,count)
+ax.set_title('# of Purchases by Category')
+
+plt.show()
