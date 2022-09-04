b8e2b7765bbd1652955853b5ed997f72082baad8
Piramol Krishnan <piramolkk@gmail.com>
2022-09-04 22:22:47 +0800

    EDIT: Initialised sum_expenses and expenses correctly

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -2,7 +2,8 @@ class BudgetList:
     def __init__(self,budget):
         self.budget = budget
         self.sum_expenses = 0
-        self.expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
         self.overages = []
 
     def append(self,item):
