85c5dbfa7b47c1350beedb52f8df4d15e1398e1e
Piramol Krishnan <piramolkk@gmail.com>
2022-09-05 23:48:21 +0800

    ADD: created for loop for subset equality test

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -10,6 +10,7 @@ def main():
         print('Sets are NOT equal by == test')
 
     for a,b in zip(divided_for_loop, divided_set_comp):
-        pass
+        if not(a.issubset(b) & b.issubset(a)):
+            print("Sets are NOT equal by subset test")
 if __name__ == "__main__":
     main()
